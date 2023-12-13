b2beacc623132ec1c8e39a6f3c845f568e535dc5
Ramazan Guner <ramazan.guner@haud.com>
2023-12-13 14:55:56 +0300

    addInventoryWithCheck - transactional

--- src/main/java/com/guner/springbootjpaaudit/service/InventoryService.java
+++ src/main/java/com/guner/springbootjpaaudit/service/InventoryService.java
@@ -3,18 +3,10 @@ package com.guner.springbootjpaaudit.service;
 
 import com.guner.springbootjpaaudit.entity.Inventory;
 import com.guner.springbootjpaaudit.entity.InventoryOwner;
-import com.guner.springbootjpaaudit.entity.UserInfo;
-import com.guner.springbootjpaaudit.model.UserInfoDetails;
 import com.guner.springbootjpaaudit.repository.InventoryOwnerRepository;
 import com.guner.springbootjpaaudit.repository.InventoryRepository;
-import com.guner.springbootjpaaudit.repository.UserInfoRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
